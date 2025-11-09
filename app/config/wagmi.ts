68921cba9ca9dd9b1cd1c9fddaaab0c4a0692b60
anurag-p6 <pandeyanurag3359@gmail.com>
2025-11-10 00:54:21 +0530

    arbitrumSepolia onlu

--- app/config/wagmi.ts
+++ app/config/wagmi.ts
@@ -4,6 +4,6 @@ import { arbitrum, arbitrumSepolia, polygon, optimism, base } from 'wagmi/chains
 export const config = getDefaultConfig({
   appName: 'Data DAO',
   projectId: 'a2e7a700058c0ec2bf3705bb43a7c0f5', // Get this from WalletConnect Cloud
-  chains: [arbitrumSepolia, arbitrum, polygon, optimism, base],
+  chains: [arbitrumSepolia],
   ssr: true, // If your dApp uses server side rendering (SSR)
 });
